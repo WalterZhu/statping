d06219d6ec4cb008d9b5502eb9e8a1b50790c026
walter <hqzhu0461@163.com>
2020-11-14 18:03:34 +0800

    优化测试环境

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,27 +1,12 @@
 module.exports = {
     filenameHashing: true,
-    // productionTip: process.env.NODE_ENV !== 'production',
-    // devtools: process.env.NODE_ENV !== 'production',
-    // performance: process.env.NODE_ENV !== 'production',
+    outputDir: '../source/dist/',
     devServer: {
-        disableHostCheck: true,
-        proxyTable: {
-            '/api': {
-                logLevel: 'debug',
-                target: 'http://0.0.0.0:8585',
+        proxy: {
+            '/api' : {
+                target: 'http://127.0.0.1:8080',
                 changeOrigin: true,
-                pathRewrite: {
-                    '^/api': ''
-                }
             },
-            '/oauth': {
-                logLevel: 'debug',
-                target: 'http://0.0.0.0:8585',
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/oauth': ''
-                }
-            }
         }
-    },
+    }
 };
--- go.mod
+++ go.mod
@@ -12,8 +12,8 @@ require (
 	github.com/foomo/tlsconfig v0.0.0-20180418120404-b67861b076c9
 	github.com/getsentry/sentry-go v0.5.1
 	github.com/go-mail/mail v2.3.1+incompatible
-	github.com/go-redis/cache/v8 v8.0.0
-	github.com/go-redis/redis/v8 v8.1.3
+	github.com/go-redis/cache/v8 v8.0.0 // indirect
+	github.com/go-redis/redis/v8 v8.1.3 // indirect
 	github.com/gorilla/mux v1.7.4
 	github.com/hako/durafmt v0.0.0-20200605151348-3a43fc422dd9
 	github.com/jinzhu/gorm v1.9.12
