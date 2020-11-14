a585dba6bdbe639acad2dbd4f4e4141e5b6acf2b
WalterZhu <hqzhu0461@163.com>
2020-11-14 09:56:25 +0800

    优化vue打包

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,59 +1,59 @@
 <template>
-  <div id="app">
-    <router-view/>
-    <Footer v-if="$route.path !== '/setup'"/>
-  </div>
+    <div id="app">
+        <router-view/>
+        <Footer v-if="$route.path !== '/setup'"/>
+    </div>
 </template>
 
 <script>
-  const Footer = () => import(/* webpackChunkName: "index" */ "./components/Index/Footer");
+    const Footer = () => import(/* webpackChunkName: "index" */ './components/Index/Footer');
 
-  export default {
-    name: 'app',
-    components: {
-      Footer
-    },
-    data() {
-      return {
-        loaded: false,
-        version: "",
-      }
-    },
-    computed: {
-      core() {
-        return this.$store.getters.core
-      }
-    },
-    async beforeMount() {
-      await this.$store.dispatch('loadCore')
+    export default {
+        name: 'App',
+        components: {
+            Footer
+        },
+        data() {
+            return {
+                loaded: false,
+                version: '',
+            };
+        },
+        computed: {
+            core() {
+                return this.$store.getters.core;
+            }
+        },
+        async beforeMount() {
+            await this.$store.dispatch('loadCore');
 
-      this.$i18n.locale = this.core.language || "en";
-      // this.$i18n.locale = "ru";
+            this.$i18n.locale = this.core.language || 'en';
+            // this.$i18n.locale = "ru";
 
-      if (!this.core.setup) {
-        this.$router.push('/setup')
-      }
-      if (this.$route.path !== '/setup') {
-        if (this.$store.state.admin) {
-          await this.$store.dispatch('loadAdmin')
-        } else {
-          await this.$store.dispatch('loadRequired')
+            if (!this.core.setup) {
+                this.$router.push('/setup');
+            }
+            if (this.$route.path !== '/setup') {
+                if (this.$store.state.admin) {
+                    await this.$store.dispatch('loadAdmin');
+                } else {
+                    await this.$store.dispatch('loadRequired');
+                }
+                this.loaded = true;
+            }
+        },
+        async mounted() {
+            if (this.$route.path !== '/setup') {
+                if (this.$store.state.admin) {
+                    this.logged_in = true;
+                    // await this.$store.dispatch('loadAdmin')
+                }
+            }
         }
-        this.loaded = true
-      }
-    },
-    async mounted() {
-      if (this.$route.path !== '/setup') {
-        if (this.$store.state.admin) {
-          this.logged_in = true
-          // await this.$store.dispatch('loadAdmin')
-        }
-      }
-    }
-  }
+    };
 </script>
 
 <style lang="scss">
     @import "./assets/css/bootstrap.min.css";
-    @import "./assets/scss/index";
+    @import "./assets/scss/index.scss";
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,23 +1,23 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import VueApexCharts from 'vue-apexcharts'
-import VueObserveVisibility from 'vue-observe-visibility'
-import VueClipboard from 'vue-clipboard2'
-import VueCookies from 'vue-cookies'
-import VueI18n from 'vue-i18n'
-import * as Sentry from "@sentry/browser";
-import * as Integrations from "@sentry/integrations";
-import router from './routes'
-import "./mixin"
-import "./icons"
-import store from './store'
-import language from './languages'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueApexCharts from 'vue-apexcharts';
+import VueObserveVisibility from 'vue-observe-visibility';
+import VueClipboard from 'vue-clipboard2';
+import VueCookies from 'vue-cookies';
+import VueI18n from 'vue-i18n';
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
+import router from './routes';
+import './mixin';
+import './icons';
+import store from './store';
+import language from './languages';
 
-const errorReporter = "https://bed4d75404924cb3a799e370733a1b64@sentry.statping.com/3"
+const errorReporter = 'https://bed4d75404924cb3a799e370733a1b64@sentry.statping.com/3';
 
-const App = () => import(/* webpackChunkName: "index" */ '@/App.vue')
+const App = () => import(/* webpackChunkName: "index" */ '@/App.vue');
 
-Vue.component('apexchart', VueApexCharts)
+Vue.component('apexchart', VueApexCharts);
 
 Vue.use(VueClipboard);
 Vue.use(VueRouter);
@@ -26,24 +26,24 @@ Vue.use(VueCookies);
 Vue.use(VueI18n);
 
 const i18n = new VueI18n({
-  fallbackLocale: "en",
-  messages: language
+    fallbackLocale: 'en',
+    messages: language
 });
 
-Vue.$cookies.config('3d')
+Vue.$cookies.config('3d');
 
 Sentry.init({
-  dsn: errorReporter,
-  integrations: [new Integrations.Vue({Vue, attachProps: true, logErrors: true})],
+    dsn: errorReporter,
+    integrations: [ new Integrations.Vue({ Vue, attachProps: true, logErrors: true }) ],
 });
 
-Vue.config.productionTip = process.env.NODE_ENV !== 'production'
-Vue.config.devtools = process.env.NODE_ENV !== 'production'
-Vue.config.performance = process.env.NODE_ENV !== 'production'
+Vue.config.productionTip = process.env.NODE_ENV !== 'production';
+Vue.config.devtools = process.env.NODE_ENV !== 'production';
+Vue.config.performance = process.env.NODE_ENV !== 'production';
 
 new Vue({
-  router,
-  store,
-  i18n,
-  render: h => h(App),
-}).$mount('#app')
+    router,
+    store,
+    i18n,
+    render: h => h(App),
+}).$mount('#app');
